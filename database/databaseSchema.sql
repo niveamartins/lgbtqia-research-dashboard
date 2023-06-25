f5977313e1cc62fc20dca02978bd655e3a68673e
Nivea Martins <niveamartins@dcc.ufrj.br>
2023-06-25 01:13:54 +0000

    fix: improving database arch

--- database/databaseSchema.sql
+++ database/databaseSchema.sql
@@ -1,19 +1,21 @@
+CREATE TABLE Categories (
+    category_id int NOT NULL AUTO_INCREMENT,
+    category VARCHAR(100) NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
 CREATE TABLE Questions (
     question_id int NOT NULL AUTO_INCREMENT,
     question TEXT(500) NOT NULL,
-    PRIMARY KEY (question_id)
+    category_id int NOT NULL,
+    PRIMARY KEY (question_id),
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id),
 );
 
 CREATE TABLE Answers (
     answer_id int NOT NULL AUTO_INCREMENT,
     answer VARCHAR(255) NOT NULL,
     PRIMARY KEY (answer_id)
-);
-
-CREATE TABLE Categories (
-    category_id int NOT NULL AUTO_INCREMENT,
-    category VARCHAR(100) NOT NULL,
-    PRIMARY KEY (category_id)
 );
 
 CREATE TABLE Groups (
@@ -30,13 +32,12 @@ CREATE TABLE Countries (
 
 CREATE TABLE AnswerQuestions (
     answerQuestion_id int NOT NULL AUTO_INCREMENT,
-    category_id int NOT NULL,
     question_id int NOT NULL,
     group_id int NOT NULL,
     answer_id int NOT NULL,
     country_id int NOT NULL,
+  	percent float NOT NULL,
     PRIMARY KEY (answerQuestion_id),
-    FOREIGN KEY (category_id) REFERENCES Categories(category_id),
   	FOREIGN KEY (question_id) REFERENCES Questions(question_id),
     FOREIGN KEY (group_id) REFERENCES Groups(group_id),
   	FOREIGN KEY (answer_id) REFERENCES Answers(answer_id),
